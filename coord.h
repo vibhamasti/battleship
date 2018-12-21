1155da0f393b42474b8ebafdbdf14f9a11d35adc
Vibha Masti <vsmasti@gmail.com>
2018-12-21 10:43:26 +0530

    added default and parameterised constructors

--- coord.h
+++ coord.h
@@ -10,13 +10,26 @@
 
 struct Coord {
     int x, y;
-    void input() {
-        std::cout << "X: ";
-        std::cin >> x;
-        std::cout << "Y: ";
-        std::cin >> y;
-    }
+    Coord();                    // default constructor
+    Coord(int, int);            // parameterised constructor
+    void input();               // inputs coordinate values
 };
 
+void Coord::input() {
+    std::cout << "X: ";
+    std::cin >> x;
+    std::cout << "Y: ";
+    std::cin >> y;
+}
+
+Coord::Coord() {
+    x = 0;
+    y = 0;
+}
+
+Coord::Coord(int a, int b) {
+    x = a;
+    y = b;
+}
 
 #endif /* coord_h */
