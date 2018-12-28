326fec965d00252f857f371bf0d27b3c731d9d70
Vibha Masti <vsmasti@gmail.com>
2018-12-28 17:21:35 +0530

    fixed bug by calling by reference - update headers

--- game.h
+++ game.h
@@ -23,13 +23,13 @@ public:
     // game initialise functions
     void inputPlayers();
     void printRules();
-    void inputPlayerData(Player);
-    void checkFile(Player);                         // checks file for name and high score
+    void inputPlayerData(Player&);
+    void checkFile(Player&);                         // checks file for name and high score
 
     // game play functions
     void play();                                    // play the game by playing multiple rounds
-    void playRound(Player, Player);                 // play one round of hitting/scoring/printing
-    void updateScores(Player, Player, Coord);       // TODO: define
+    void playRound(Player&, Player&);                 // play one round of hitting/scoring/printing
+    void updateScores(Player&, Player&, Coord&);       // TODO: define
     bool inPlay();
     void saveScore(char *, int);                    // save high score onto file      
 };
