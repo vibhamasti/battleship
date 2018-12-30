a4da4bdcc69bca22f3899fec9ca3d851ace827a6
Vibha Masti <vsmasti@gmail.com>
2018-12-30 18:20:21 +0530

    changed header for inputPlayerData to take two Player arguments

--- game.h
+++ game.h
@@ -23,13 +23,14 @@ public:
     // game initialise functions
     void inputPlayers();
     void printRules();
-    void inputPlayerData(Player&);
-    void checkFile(Player&);                         // checks file for name and high score
+    void inputPlayerData(Player&, Player&);         // input player name and ships
+    void checkFile(Player&);                        // checks file for name and high score
+    bool nameExists(char*);                         // checks if player name exists
 
     // game play functions
     void play();                                    // play the game by playing multiple rounds
-    void playRound(Player&, Player&);                 // play one round of hitting/scoring/printing
-    void updateScores(Player&, Player&, Coord&);       // TODO: define
+    void playRound(Player&, Player&);               // play one round of hitting/scoring/printing
+    void updateScores(Player&, Player&, Coord&);    // TODO: define
     bool inPlay();
     void saveScore(char *, int);                    // save high score onto file      
 };
